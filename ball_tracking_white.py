df382e567425994ad22333c18e7509316b6ac8d3
Cory Gryphon <corygryphon@gmail.com>
2019-05-02 21:47:27 -0500

    Add files via upload

--- ball_tracking_white.py
+++ ball_tracking_white.py
@@ -8,13 +8,18 @@ import time
 ap = argparse.ArgumentParser()
 ap.add_argument("-v", "--video", help="path to the (optional) video file")
 args = vars(ap.parse_args())
+#convert RGB to HSV
+r = 232
+g = 255
+b = 66
+color = np.uint8([[[b, g, r]]])
+rgb_hsv = cv2.cvtColor(color, cv2.COLOR_BGR2HSV)
+print(rgb_hsv[0][0][0]-10,100,100)
+#greenLower = (22, 57, 191)
+#greenUpper = (76, 193, 255)
 
-#greenLower = (0, 0, 123)
-#greenUpper = (29, 111, 255)
-
-greenLower = (29, 60, 136)
-greenUpper = (35, 181, 255)
-
+#greenLower = (29, 60, 136)## pop.mp4 colors
+#greenUpper = (35, 181, 255)
 
 #greenLower = (29, 86, 6)
 #greenUpper = (64, 255, 255)
@@ -30,33 +35,12 @@ while True:
     time.sleep(.1)
     if args.get("video") and not grabbed:
         break
-
     frame = imutils.resize(frame, width=600)
-    ##gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    # detect circles in the image
-    ##circles = cv2.HoughCircles(gray, cv2.cv.CV_HOUGH_GRADIENT, 1.2, 100)
-     
-    # ensure at least some circles were found
-    ##if circles is not None:
-        # convert the (x, y) coordinates and radius of the circles to integers
-    ##    circles = np.round(circles[0, :]).astype("int")
-     
-        # loop over the (x, y) coordinates and radius of the circles
-    ##    for (x, y, r) in circles:
-            # draw the circle in the output image, then draw a rectangle
-            # corresponding to the center of the circle
-    ##        cv2.circle(output, (x, y), r, (0, 255, 0), 4)
-    ##        cv2.rectangle(output, (x - 5, y - 5), (x + 5, y + 5), (0, 128, 255), -1)
-     
-        # show the output image
-    ##    cv2.imshow("output", np.hstack([image, output]))
-    ##    cv2.waitKey(0)
-
-
-
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    lowerLimit = ((rgb_hsv[0][0][0])-10,100,100)
+    upperLimit = ((rgb_hsv[0][0][0])+10,255,255)
     kernel = np.zeros((2,2), np.uint8)
-    mask = cv2.inRange(hsv, greenLower, greenUpper)
+    mask = cv2.inRange(hsv, lowerLimit, upperLimit)
     #mask = cv2.erode(mask, kernel)
     mask = cv2.erode(mask, None, iterations=2)
     mask = cv2.dilate(mask, None, iterations=2)
@@ -64,7 +48,7 @@ while True:
     cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
     center = None
 
-    if len(cnts) > 0:
+    if len(cnts) > 10:
         c = max(cnts, key=cv2.contourArea)
         ((x, y), radius) = cv2.minEnclosingCircle(c)
 
