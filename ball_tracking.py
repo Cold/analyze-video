a334c3d028c209185c9411869d1d332fb28846a3
Holden <holden.c.smith@healthpartners.com>
2019-10-19 16:18:55 -0500

    clean up code as per pycodestyle recommendations.

--- ball_tracking.py
+++ ball_tracking.py
@@ -1,5 +1,4 @@
-from collections import deque
-import numpy as np
+#!/usr/bin/env python3
 import argparse
 import imutils
 import cv2
@@ -11,8 +10,8 @@ args = vars(ap.parse_args())
 greenLower = (29, 60, 136)
 greenUpper = (35, 181, 255)
 
-#greenLower = (29, 86, 6)
-#greenUpper = (64, 255, 255)
+# greenLower = (29, 86, 6)
+# greenUpper = (64, 255, 255)
 
 if not args.get("video", False):
     camera = cv2.VideoCapture(0)
@@ -26,27 +25,22 @@ while True:
         break
 
     frame = imutils.resize(frame, width=600)
-    ##gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    # #gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     # detect circles in the image
-    ##circles = cv2.HoughCircles(gray, cv2.cv.CV_HOUGH_GRADIENT, 1.2, 100)
-     
+    # #circles = cv2.HoughCircles(gray, cv2.cv.CV_HOUGH_GRADIENT, 1.2, 100)
     # ensure at least some circles were found
-    ##if circles is not None:
-        # convert the (x, y) coordinates and radius of the circles to integers
-    ##    circles = np.round(circles[0, :]).astype("int")
-     
-        # loop over the (x, y) coordinates and radius of the circles
-    ##    for (x, y, r) in circles:
-            # draw the circle in the output image, then draw a rectangle
-            # corresponding to the center of the circle
-    ##        cv2.circle(output, (x, y), r, (0, 255, 0), 4)
-    ##        cv2.rectangle(output, (x - 5, y - 5), (x + 5, y + 5), (0, 128, 255), -1)
-     
-        # show the output image
-    ##    cv2.imshow("output", np.hstack([image, output]))
-    ##    cv2.waitKey(0)
-
-
+    # #if circles is not None:
+    # convert the (x, y) coordinates and radius of the circles to integers
+    # #    circles = np.round(circles[0, :]).astype("int")
+    # loop over the (x, y) coordinates and radius of the circles
+    # #    for (x, y, r) in circles:
+    # draw the circle in the output image, then draw a rectangle
+    # corresponding to the center of the circle
+    # #        cv2.circle(output, (x, y), r, (0, 255, 0), 4)
+    # #        cv2.rectangle(output, (x - 5, y - 5), (x + 5, y + 5), (0, 128, 255), -1)
+    # show the output image
+    # #    cv2.imshow("output", np.hstack([image, output]))
+    # #    cv2.waitKey(0)
 
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
@@ -74,4 +68,3 @@ while True:
 
 camera.release()
 cv2.destroyAllWindows()
-
